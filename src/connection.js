588967d578f9acc1f865f2a5410f389560a88a32
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-09-16 17:54:32 -0300

    fix connection error

--- src/connection.js
+++ src/connection.js
@@ -3,8 +3,8 @@ const { mongodb } = require('./config.js');
 
 const connection = mongoose
   .connect(
-    `mongodb://${mongodb.host}/${mongodb.database}`
-    // `mongodb+srv://${mongodb.user}:${mongodb.password}@${mongodb.host}/${mongodb.database}?retryWrites=true&w=majority`
+    // `mongodb://${mongodb.host}/${mongodb.database}`
+    `mongodb+srv://${mongodb.user}:${mongodb.password}@${mongodb.host}/${mongodb.database}?retryWrites=true&w=majority`
   )
   .then((db) => {
     console.log(`Connected to MongoDB in:  ${mongodb.host}`);
